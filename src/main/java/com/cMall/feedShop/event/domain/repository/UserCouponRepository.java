0a9a92b04b3b2e71c7c539fbfff27045e3a855b3
정승민 <67104714+Codemaniac7@users.noreply.github.com>
2025-08-04 12:42:53 +0900

    주석 처리

--- src/main/java/com/cMall/feedShop/event/domain/repository/UserCouponRepository.java
+++ src/main/java/com/cMall/feedShop/event/domain/repository/UserCouponRepository.java
@@ -1,14 +1,19 @@
-package com.cMall.feedShop.user.domain.repository;
-
-import com.cMall.feedShop.user.domain.enums.UserCouponStatus;
-import com.cMall.feedShop.user.domain.model.User;
-import com.cMall.feedShop.event.domain.model.UserCoupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface UserCouponRepository extends JpaRepository<UserCoupon,Long> {
-    List<UserCoupon> findByUserAndCouponStatus(User user, UserCouponStatus couponStatus);
-    Optional<UserCoupon> findByCouponCode(String couponCode);
-}
+//package com.cMall.feedShop.event.domain.repository;
+//
+//import com.cMall.feedShop.user.domain.enums.UserCouponStatus;
+//import com.cMall.feedShop.user.domain.model.User;
+//import com.cMall.feedShop.user.domain.model.UserCoupon;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface UserCouponRepository extends JpaRepository<UserCoupon,Long> {
+//    // User 객체를 통해 찾는 방식
+//    List<UserCoupon> findByUserAndCouponStatus(User user, UserCouponStatus couponStatus);
+//
+//    // User ID를 통해 찾는 방식 (더 범용적으로 사용될 수 있음)
+//    List<UserCoupon> findByUserIdAndCouponStatus(Long userId, UserCouponStatus couponStatus);
+//
+//    Optional<UserCoupon> findByCouponCode(String couponCode);
+//}
